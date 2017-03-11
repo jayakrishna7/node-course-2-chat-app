7a4a0fc0a190c4978f0bd27e6c0795a13313304e
JayaKrishna Nannapaneni <JayaKrishna Nannapaneni>
2017-03-11 06:39:24 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-socket.emit('createMessage', {
-  from:'JK',
-  text:'Hey.This is JK.'
-});
-
 });
 
 socket.on('disconnect', function() {
