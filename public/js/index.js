7dd62b8154b1b9409554ce7f48b812e1c8220edf
JayaKrishna Nannapaneni <JayaKrishna Nannapaneni>
2017-03-12 12:01:33 +0530

    Add form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,27 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('newMessage', message);
+ var li=jQuery('<li></li>');
+ li.text('Admin : welcome to chat app.');
+ li.text('user:message send');
+
+jQuery('#messages').append(li);
+});
+
+socket.emit('createMessage', {
+  from:'Frank',
+  text:'Hi'
+}, function(data) {
+  console.log('Got it', data);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
