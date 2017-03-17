ca520449bb3adfc71a7bc7f40f855ed80e0bd49e
JayaKrishna Nannapaneni <JayaKrishna Nannapaneni>
2017-03-17 19:37:02 +0530

    Add join page that submits to chat.html

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function  scrollToBottom() {
+  //selectors
+var messages = jQuery('#messages');
+var newMessage = messages.children('li:last-child');
+  //Heights
+var clientHeight = messages.prop('clientHeight');
+var scrollTop = messages.prop('scrollTop');
+var scrollHeight = messages.prop('scrollHeight');
+var newMessageHeight = newMessage.innerHeight();
+var lastMessageHeight = newMessage.prev().innerHeight();
+
+if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+  messages.scrollTop(scrollHeight);
+}
+}
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -18,11 +34,7 @@ var formattedTime = moment(message.createdAt).format('h:mm a');
   });
 
   jQuery('#messages').append(html);
-
- //var li=jQuery('<li></li>');
- //li.text(`${messages.from} ${formattedTime}: ${messages.text}`);
-//
-//jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.emit('newLocationMessage', function(message) {
@@ -35,7 +47,7 @@ socket.emit('newLocationMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
-
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
